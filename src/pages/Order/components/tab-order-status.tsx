01c1b7f6b4b6c5a6c23d3ce45d8f2bac86044391
phatnguyentandev <phatanphat@gmail.com>
2025-06-16 00:35:14 +0700

    fix một số lỗi

--- src/pages/Order/components/tab-order-status.tsx
+++ src/pages/Order/components/tab-order-status.tsx
@@ -18,6 +18,8 @@ import { format } from "date-fns";
 import DialogConfirmOrder from "./dialog-confirm-order";
 import { toast, ToastContainer } from "react-toastify";
 import Swal from "sweetalert2";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 
 const OrderPage = () => {
   const [activeTab, setActiveTab] = useState("Processing");
@@ -25,15 +27,17 @@ const OrderPage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const [editId, setEditId] = useState("");
+  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date(Date.now()));
 
   const tabs = ["Processing", "Shipping", "Returned", "Done", "Cancelled"];
 
   const LoadOrders = async () => {
     setIsLoading(true);
     try {
+      console.log(selectedDate?.toLocaleDateString());
       const response: ApiResponse = await clientAPI
         .service("orders/manager")
-        .find(`orderStatus=${activeTab}`);
+        .find(`orderStatus=${activeTab}&latestStatusDay=${selectedDate?.toLocaleDateString()}`);
       setDataOrders(response.result || []);
     } catch (error) {
       console.error("Error loading orders:", error);
@@ -45,7 +49,7 @@ const OrderPage = () => {
 
   useEffect(() => {
     LoadOrders();
-  }, [activeTab]);
+  }, [activeTab, selectedDate]);
 
   const filteredOrders = dataOrders.filter((order) =>
     order.orderItems.some(
@@ -77,7 +81,11 @@ const OrderPage = () => {
           Swal.fire("Đã hủy!", "Đơn hàng của bạn đã được hủy.", "success");
           LoadOrders();
         } else {
-          Swal.fire("Lỗi!", "Không thể hủy đơn hàng. Vui lòng thử lại.", "error");
+          Swal.fire(
+            "Lỗi!",
+            "Không thể hủy đơn hàng. Vui lòng thử lại.",
+            "error"
+          );
         }
       } catch (error) {
         Swal.fire("Lỗi!", "Không thể kết nối đến máy chủ.", "error");
@@ -108,155 +116,190 @@ const OrderPage = () => {
   };
 
   const handleCreateOrderGHN = async (id: string) => {
-    const confirmDelete = window.confirm("Are you sure you want to create order GHN?");
-    if (!confirmDelete) return;
+    const result = await Swal.fire({
+      title: "Xác nhận",
+      text: "Bạn có chắc chắn muốn tạo đơn hàng GHN?",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonText: "Tạo đơn",
+      cancelButtonText: "Hủy",
+    });
+
+    if (!result.isConfirmed) return;
+
     try {
       await clientAPI.service(`Orders/${id}/create-order-ghn`).create("");
-      toast.success("Create order GHN successfully");
+      await Swal.fire("Thành công", "Tạo đơn hàng GHN thành công!", "success");
       LoadOrders();
+    } catch (error: any) {
+      Swal.fire(
+        "Lỗi",
+        error?.response?.data?.errorMessages?.[0] || "Đã xảy ra lỗi",
+        "error"
+      );
     }
-    catch (error: any) {
-      toast.error(error?.response?.data?.errorMessages[0]);
-    }
-  }
+  };
 
   const triggerShowInfoOrder = async (orderId: string) => {
     try {
-      var data: ApiResponse = await clientAPI.service(`orders/${orderId}`).find();
+      var data: ApiResponse = await clientAPI
+        .service(`orders/${orderId}`)
+        .find();
 
       toast.success(
         <div>
-          <strong>Đơn vị vận chuyển:</strong> {data.result?.transferService} <br />
+          <strong>Đơn vị vận chuyển:</strong> {data.result?.transferService}{" "}
+          <br />
           <strong>Mã vận đơn:</strong> {data.result?.addressCode}
         </div>
       );
-    } catch (e) {
-    }
-  }
+    } catch (e) {}
+  };
 
   return (
-    <Box
-      sx={{ width: "100%", overflow: "hidden", borderRadius: 3, boxShadow: 5 }}
-    >
-      <Box sx={{ bgcolor: "#fff", borderRadius: 2, boxShadow: 2 }}>
-        {/* Tabs */}
-        <Tabs
-          value={activeTab}
-          onChange={(e, newValue) => setActiveTab(newValue)}
-          variant="fullWidth"
-          textColor="secondary"
-          indicatorColor="secondary"
-        >
-          {tabs.map((tab) => (
-            <Tab key={tab} label={tab} value={tab} />
-          ))}
-        </Tabs>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Box
+        sx={{
+          width: "100%",
+          overflow: "hidden",
+          borderRadius: 3,
+          boxShadow: 5,
+        }}
+      >
+        <Box sx={{ bgcolor: "#fff", borderRadius: 2, boxShadow: 2 }}>
+          {/* Tabs */}
+          <Tabs
+            value={activeTab}
+            onChange={(e, newValue) => setActiveTab(newValue)}
+            variant="fullWidth"
+            textColor="secondary"
+            indicatorColor="secondary"
+          >
+            {tabs.map((tab) => (
+              <Tab key={tab} label={tab} value={tab} />
+            ))}
+          </Tabs>
 
-        {/* Search */}
-        <Box sx={{ p: 2 }}>
-          <TextField
-            fullWidth
-            label="Tìm kiếm theo tên sản phẩm hoặc ID"
-            variant="outlined"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-        </Box>
+          {/* Search */}
+          <Box sx={{ display: "flex", gap: 2, p: 2 }}>
+            <TextField
+              sx={{ flex: 1, minWidth: 3 / 4 }}
+              label="Tìm kiếm theo tên sản phẩm hoặc ID"
+              variant="outlined"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <DatePicker
+              label="Chọn ngày"
+              value={selectedDate}
+              onChange={(newDate) => setSelectedDate(newDate)}
+              format="dd/MM/yyyy"
+              slotProps={{
+                textField: {
+                  fullWidth: true,
+                  variant: "outlined",
+                },
+              }}
+              sx={{ flex: 1 }}
+            />
+          </Box>
 
-        {/* Orders */}
-        <Box sx={{ p: 2 }}>
-          <Typography variant="h6">{activeTab} Orders</Typography>
-          <Typography variant="body2" color="text.secondary">
-            Hiển thị danh sách đơn hàng trong trạng thái "{activeTab}".
-          </Typography>
-
-          {isLoading ? (
-            <Box textAlign="center" mt={4}>
-              <CircularProgress />
-              <Typography>Đang tải...</Typography>
-            </Box>
-          ) : filteredOrders.length === 0 ? (
-            <Typography textAlign="center" color="text.secondary" mt={4}>
-              Không có đơn hàng trong trạng thái "{activeTab}".
+          {/* Orders */}
+          <Box sx={{ p: 2 }}>
+            <Typography variant="h6">{activeTab} Orders</Typography>
+            <Typography variant="body2" color="text.secondary">
+              Hiển thị danh sách đơn hàng trong trạng thái "{activeTab}".
             </Typography>
-          ) : (
-            filteredOrders.map((order, index) => (
-              <Card key={index} sx={{ mt: 3 }}>
-                <CardContent>
-                  <Grid container justifyContent="space-between">
-                    <Grid>
-                      <Typography>
-                        Trạng thái đơn hàng:{" "}
-                        <Typography component="span" color="success.main">
-                          {order.orderStatus}
-                        </Typography>
-                      </Typography>
-                    </Grid>
-                    <Grid>
-                      <Typography>
-                        Ngày đặt hàng:{" "}
-                        <Typography component="span" color="primary">
-                          {order?.orderPaidTime
-                            ? format(
-                              new Date(order?.orderPaidTime),
-                              "dd/MM/yyyy"
-                            )
-                            : "Chưa có thông tin"}
-                        </Typography>
-                      </Typography>
-                    </Grid>
-                  </Grid>
 
-                  <Box mt={2}>
-                    {order.orderItems.map((item, idx) => (
-                      <Grid
-                        key={idx}
-                        container
-                        spacing={2}
-                        alignItems="center"
-                        sx={{ mb: 1, borderBottom: "1px solid #eee", pb: 1 }}
-                      >
-                        <Grid>
-                          <img
-                            src={
-                              item.imageItemUrl ||
-                              "https://placehold.co/600x400"
-                            }
-                            alt={item.nameItem}
-                            width={64}
-                            height={64}
-                            style={{ objectFit: "cover", borderRadius: 8 }}
-                          />
-                        </Grid>
-                        <Grid>
-                          <Typography>{item.nameItem}</Typography>
-                          <Typography variant="body2">
-                            x{item.quantity}
+            {isLoading ? (
+              <Box textAlign="center" mt={4}>
+                <CircularProgress />
+                <Typography>Đang tải...</Typography>
+              </Box>
+            ) : filteredOrders.length === 0 ? (
+              <Typography textAlign="center" color="text.secondary" mt={4}>
+                Không có đơn hàng trong trạng thái "{activeTab}".
+              </Typography>
+            ) : (
+              filteredOrders.map((order, index) => (
+                <Card key={index} sx={{ mt: 3 }}>
+                  <CardContent>
+                    <Grid container justifyContent="space-between">
+                      <Grid>
+                        <Typography>
+                          Trạng thái đơn hàng:{" "}
+                          <Typography component="span" color="success.main">
+                            {order.orderStatus}
                           </Typography>
-                        </Grid>
-                        <Grid>
-                          <Typography color="orange">{Number(item.price).toLocaleString("vi-VN")} đ</Typography>
-                        </Grid>
+                        </Typography>
                       </Grid>
-                    ))}
+                      <Grid>
+                        <Typography>
+                          Ngày đặt hàng:{" "}
+                          <Typography component="span" color="primary">
+                            {order?.orderPaidTime
+                              ? format(
+                                  new Date(order?.orderPaidTime),
+                                  "dd/MM/yyyy"
+                                )
+                              : "Chưa có thông tin"}
+                          </Typography>
+                        </Typography>
+                      </Grid>
+                    </Grid>
 
-                    <Typography fontWeight="bold">
-                      Tổng tiền hóa đơn: {Number(order.totalPrice).toLocaleString("vi-VN")} đ
-                    </Typography>
-                    <Typography fontWeight="bold">
-                      Địa chỉ nhận hàng: {order.shippingAddress}
-                    </Typography>
-                  </Box>
+                    <Box mt={2}>
+                      {order.orderItems.map((item, idx) => (
+                        <Grid
+                          key={idx}
+                          container
+                          spacing={2}
+                          alignItems="center"
+                          sx={{ mb: 1, borderBottom: "1px solid #eee", pb: 1 }}
+                        >
+                          <Grid>
+                            <img
+                              src={
+                                item.imageItemUrl ||
+                                "https://placehold.co/600x400"
+                              }
+                              alt={item.nameItem}
+                              width={64}
+                              height={64}
+                              style={{ objectFit: "cover", borderRadius: 8 }}
+                            />
+                          </Grid>
+                          <Grid>
+                            <Typography>{item.nameItem}</Typography>
+                            <Typography variant="body2">
+                              x{item.quantity}
+                            </Typography>
+                          </Grid>
+                          <Grid>
+                            <Typography color="orange">
+                              {Number(item.price).toLocaleString("vi-VN")} đ
+                            </Typography>
+                          </Grid>
+                        </Grid>
+                      ))}
 
-                  {(order.orderStatus === "Pending" ||
-                    order.orderStatus === "Processing") && (
+                      <Typography fontWeight="bold">
+                        Tổng tiền hóa đơn:{" "}
+                        {Number(order.totalPrice).toLocaleString("vi-VN")} đ
+                      </Typography>
+                      <Typography fontWeight="bold">
+                        Địa chỉ nhận hàng: {order.shippingAddress}
+                      </Typography>
+                    </Box>
+
+                    {(order.orderStatus === "Pending" ||
+                      order.orderStatus === "Processing") && (
                       <Box
                         mt={2}
                         display="flex"
                         justifyContent="flex-end"
                         gap={2}
                       >
-
                         <Button
                           variant="contained"
                           color="success"
@@ -282,23 +325,30 @@ const OrderPage = () => {
                         </Button>
                       </Box>
                     )}
-                  {
-                    activeTab === "Shipping" && (<div className="flex justify-end">
-                      <button className="bg-green-800 rounded-md px-2 py-1 text-white" onClick={() => triggerShowInfoOrder(order.id)}>Xem thông tin vận chuyển</button>
-                    </div>)
-                  }
-                </CardContent>
-              </Card>
-            ))
-          )}
+                    {activeTab === "Shipping" && (
+                      <div className="flex justify-end">
+                        <button
+                          className="bg-green-800 rounded-md px-2 py-1 text-white"
+                          onClick={() => triggerShowInfoOrder(order.id)}
+                        >
+                          Xem thông tin vận chuyển
+                        </button>
+                      </div>
+                    )}
+                  </CardContent>
+                </Card>
+              ))
+            )}
+          </Box>
         </Box>
+        <DialogConfirmOrder
+          orderId={editId}
+          openDialogConfirmOrder={openDialogConfirmOrder}
+          onClose={() => setOpenDialogConfirmOrder(false)}
+        />
       </Box>
-      <DialogConfirmOrder
-        orderId={editId}
-        openDialogConfirmOrder={openDialogConfirmOrder}
-        onClose={() => setOpenDialogConfirmOrder(false)}
-      />
-    </Box >
+      <ToastContainer />
+    </LocalizationProvider>
   );
 };
 
